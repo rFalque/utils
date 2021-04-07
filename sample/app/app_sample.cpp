49fbd8f35939607738e02f7c9ca04ce01e95de54
rFalque <r.falque@gmail.com>
2021-04-08 09:36:11 +1000

    add sample

---
+++ sample/app/app_sample.cpp
@@ -0,0 +1,45 @@
+/*
+*   sample
+*   by R.FALQUE
+*   08/04/2021
+*/
+
+// Basics
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <Eigen/Core>
+#include <boost/filesystem.hpp>
+
+#include "utils/IO/readPLY.h"
+#include "utils/IO/writePLY.h"
+
+// plotting functions
+#include "utils/visualization/plotMesh.h"
+#include "utils/visualization/plotCloud.h"
+
+int main()
+{
+    // settings
+    bool visualization = false;
+
+    // vars
+    string ply_path = "../data/sample.ply";
+    string out_path = "../data/output.ply";
+
+    // load mesh
+    Eigen::MatrixXd V, N;
+    Eigen::MatrixXi F, C;
+
+    readPLY(ply_path, V, F, N, C);
+
+    if (visualization) {
+        plotMesh(V, F);
+        plotCloud(V);
+    }
+
+    writePLY(out_path, V, F, N, C);
+
+    return(0);
+}
