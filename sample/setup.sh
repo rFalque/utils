bc6eec27f8cb5c12cd494460cbada1926764a2a7
rFalque <r.falque@gmail.com>
2021-04-08 10:12:35 +1000

    test with low depth

--- sample/setup.sh
+++ sample/setup.sh
@@ -1,7 +1,7 @@
 cp -r sample/* ../
 cd ..
 mv 3D_tools/ utils/
-git clone --recurse-submodules https://github.com/nmwsharp/polyscope.git
+git clone --depth 1 --recurse-submodules https://github.com/nmwsharp/polyscope.git
 mkdir build
 cd build
 cmake ..
