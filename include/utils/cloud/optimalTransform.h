ca0854c9d1375837145b7c438b40a6902c3dc37e
rfalque <r.falque@gmail.com>
2022-01-21 15:49:37 +1100

    add input check

--- include/utils/cloud/optimalTransform.h
+++ include/utils/cloud/optimalTransform.h
@@ -1,13 +1,17 @@
 #ifndef OPTIMAL_TRANSFORM_H
 #define OPTIMAL_TRANSFORM_H
 
-#include <Eigen/Core>
-#include <Eigen/SVD>
 #include <Eigen/Dense>
+#include <Eigen/SVD>
 
-// see http://nghiaho.com/?page_id=671
+// following http://nghiaho.com/?page_id=671
 inline void optimal_transform(Eigen::MatrixXd A, Eigen::MatrixXd B,
                               Eigen::Matrix3d &R, Eigen::Vector3d &t) {
+  // check input size
+  if (A.rows() != 3 || B.rows() != 3 || A.cols() != B.cols() || A.cols()<3)
+    throw std::invalid_argument(
+        "Error in " + std::string(__func__) + ": wrong input size");
+
   // get centroids
   Eigen::Vector3d centroid_A = A.rowwise().mean();
   Eigen::Vector3d centroid_B = B.rowwise().mean();
@@ -18,10 +22,8 @@ inline void optimal_transform(Eigen::MatrixXd A, Eigen::MatrixXd B,
   Eigen::Matrix3d U = svd.matrixU();
   Eigen::Matrix3d V = svd.matrixV();
 
-
   // get R
   R = V * U.transpose();
-  
   if (R.determinant() < 0) {
       V.col(2) = -V.col(2);
       R = V * U.transpose();
@@ -29,7 +31,6 @@ inline void optimal_transform(Eigen::MatrixXd A, Eigen::MatrixXd B,
 
   // get t
   t = centroid_B - R*centroid_A;
-
 };
 
 #endif
