66df0ab2e9eb456f1cb2f95a5b29218da5e532d9
rFalque <r.falque@gmail.com>
2022-02-15 18:46:26 +1100

    change matrices orientations

--- include/utils/visualization/polyscopeWrapper.h
+++ include/utils/visualization/polyscopeWrapper.h
@@ -28,6 +28,7 @@ namespace visualization {
             polyscope::view::windowWidth = 1024;
             polyscope::view::windowHeight = 1024;
             polyscope::view::style = polyscope::view::NavigateStyle::Free;
+            polyscope::view::upDir = polyscope::view::UpDir::NegZUp;
     }
 
     inline void close() {
@@ -36,27 +37,31 @@ namespace visualization {
 
     // meshes
     inline void add_mesh(const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces, std::string mesh_name) {
-        polyscope::registerSurfaceMesh(mesh_name, vertices, faces);
+        polyscope::registerSurfaceMesh(mesh_name, vertices.transpose(), faces.transpose());
         polyscope::getSurfaceMesh(mesh_name)->setSurfaceColor(glm::vec3{0.1, 0.1, 1});
         polyscope::view::resetCameraToHomeView();
     }
 
-    inline void add_color_to_mesh(const Eigen::MatrixXd & colors, std::string mesh_name, std::string color_name) {
+    inline void add_colors_to_mesh(const Eigen::MatrixXd & colors, std::string mesh_name, std::string color_name) {
         if (colors.rows() != 0){
             polyscope::getSurfaceMesh(mesh_name)->addVertexColorQuantity(color_name, colors);
             polyscope::getSurfaceMesh(mesh_name)->getQuantity(color_name)->setEnabled(true);
         }
     }
+
+    inline void set_mesh_color(const Eigen::Vector3d& color, std::string mesh_name) {
+        polyscope::getSurfaceMesh(mesh_name)->setSurfaceColor(glm::vec3{color(0), color(1), color(2)});
+    }
     
     
     // cloud
     inline void add_cloud(const Eigen::MatrixXd & cloud, std::string cloud_name) {
-        polyscope::registerPointCloud(cloud_name, cloud);
+        polyscope::registerPointCloud(cloud_name, cloud.transpose());
         polyscope::getPointCloud(cloud_name)->setPointColor(glm::vec3{0.1, 0.1, 1});
         polyscope::view::resetCameraToHomeView();
     }
 
-    inline void add_color_to_cloud(const Eigen::MatrixXd & colors, std::string cloud_name, std::string color_name) {
+    inline void add_colors_to_cloud(const Eigen::MatrixXd & colors, std::string cloud_name, std::string color_name) {
         if (colors.rows() != 0){
             polyscope::getPointCloud(cloud_name)->addColorQuantity(color_name, colors);
             polyscope::getPointCloud(cloud_name)->getQuantity(color_name)->setEnabled(true);
@@ -111,8 +116,8 @@ namespace visualization {
 
     // vectors
     inline void add_vectors(const Eigen::MatrixXd& vectors_begin, const Eigen::MatrixXd& vectors_end, std::string vectors_name) {
-        polyscope::registerPointCloud(vectors_name, vectors_begin);
-        polyscope::getPointCloud(vectors_name)->addVectorQuantity("vectors", vectors_end - vectors_begin, polyscope::VectorType::STANDARD);
+        polyscope::registerPointCloud(vectors_name, vectors_begin.transpose());
+        polyscope::getPointCloud(vectors_name)->addVectorQuantity("vectors", (vectors_end - vectors_begin).transpose(), polyscope::VectorType::STANDARD);
         polyscope::getPointCloud(vectors_name)->getQuantity("vectors")->setEnabled(true);
         polyscope::view::resetCameraToHomeView();
     }
