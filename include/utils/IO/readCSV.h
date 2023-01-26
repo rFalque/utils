b91c77c5826e2a07f2e40c4dd8ece11640bed464
rFalque <r.falque@gmail.com>
2023-01-27 10:13:44 +1100

    update comment to match name

--- include/utils/IO/readCSV.h
+++ include/utils/IO/readCSV.h
@@ -9,7 +9,7 @@
 // using namespace Eigen;
 
 // from: https://stackoverflow.com/a/39146048/2562693
-// e.g., Eigen::MatrixXd matrix = load_csv<Eigen::MatrixXd>("csv_path.csv");
+// e.g., Eigen::MatrixXd matrix = read_csv<Eigen::MatrixXd>("csv_path.csv");
 template <typename M>
 M read_csv(const std::string& path) {
   std::ifstream indata;
