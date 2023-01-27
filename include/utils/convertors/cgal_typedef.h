e0376807fc77fbfb925d1511ce9fbbb8a6b96198
rFalque <r.falque@gmail.com>
2023-01-27 17:08:58 +1100

    clean up the typedef a bit

--- include/utils/convertors/cgal_typedef.h
+++ include/utils/convertors/cgal_typedef.h
@@ -7,18 +7,19 @@
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Surface_mesh.h>
 
-typedef CGAL::Simple_cartesian<double> Kernel;
-typedef Kernel::Point_3 Point;
-typedef CGAL::Surface_mesh<Point> CGAL_Mesh;
-typedef CGAL_Mesh::Vertex_index CGAL_vertex;
-typedef CGAL_Mesh::Face_index CGAL_face;
-typedef CGAL_Mesh::Vertex_iterator CGAL_vertex_iterator;
+typedef CGAL::Simple_cartesian<double>              Kernel;
+typedef Kernel::Point_3                             Point;
+typedef CGAL::Surface_mesh<Point>                   CGAL_Mesh;
+typedef CGAL_Mesh::Vertex_index                     CGAL_vertex;
+typedef CGAL_Mesh::Face_index                       CGAL_face;
+typedef CGAL_Mesh::Vertex_iterator                  CGAL_vertex_iterator;
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel CGAL_Poly_Kernel;
-typedef CGAL::Polyhedron_3<Kernel> CGAL_Poly;
-typedef CGAL_Poly::Halfedge_handle CGAL_Poly_Halfedge_handle;
-typedef CGAL_Poly::Facet_handle CGAL_Poly_Facet_handle;
-typedef CGAL_Poly::Vertex_handle CGAL_Poly_Vertex_handle;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel     CGAL_Poly_Kernel;
+typedef CGAL::Polyhedron_3<Kernel>                              CGAL_Poly;
+typedef CGAL::Polyhedron_3<Kernel>                              Polyhedron;
+typedef CGAL_Poly::Halfedge_handle                              CGAL_Poly_Halfedge_handle;
+typedef CGAL_Poly::Facet_handle                                 CGAL_Poly_Facet_handle;
+typedef CGAL_Poly::Vertex_handle                                CGAL_Poly_Vertex_handle;
 
 typedef CGAL::Mean_curvature_flow_skeletonization<CGAL_Mesh> Skeletonization;
 typedef Skeletonization::Skeleton CGAL_Skeleton;
